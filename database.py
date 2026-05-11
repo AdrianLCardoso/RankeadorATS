484efdf85fad422b58d5026c518780beaaaca559
Adrian Cardoso <adrianmenezes77@gmail.com>
2026-05-11 16:55:29 -0300

    Refactor database

--- database.py
+++ database.py
@@ -1,37 +1,36 @@
-import sqlite3
 import hashlib
+import psycopg2
 import streamlit as st
 import pandas as pd
+from datetime import datetime
 
 def conectar():
-    return sqlite3.connect('usuarios.db', check_same_thread=False)
-
-import streamlit as st
-import hashlib
+    # Busca a URL
+    return psycopg2.connect(st.secrets["DB_URL"])
 
 def criar_tabela():
     conn = conectar()
     c = conn.cursor()
+    
+    # Cria tabelas se não existirem
     c.execute('CREATE TABLE IF NOT EXISTS usuarios (email TEXT PRIMARY KEY, senha TEXT, tipo TEXT)')
     c.execute('CREATE TABLE IF NOT EXISTS logs (data TEXT, usuario TEXT, empresa TEXT, cargo TEXT, score INTEGER)')
     
-    #Busca E-mail
     meu_email = st.secrets.get("ADMIN_EMAIL") 
-    
     if meu_email:
-        c.execute('SELECT * FROM usuarios WHERE email = ?', (meu_email,))
+        c.execute('SELECT * FROM usuarios WHERE email = %s', (meu_email,))
         user = c.fetchone()
-        if user and user[2] != 'admin':
-            c.execute("UPDATE usuarios SET tipo = 'admin' WHERE email = ?", (meu_email,))
+        
+        if user:
+            if user[2] != 'admin':
+                c.execute("UPDATE usuarios SET tipo = 'admin' WHERE email = %s", (meu_email,))
+                conn.commit()
+        else:
+            senha_padrao = hash_senha("mudar_em_breve")
+            c.execute('INSERT INTO usuarios VALUES (%s, %s, %s)', (meu_email, senha_padrao, "admin"))
             conn.commit()
-    
-    c.execute('SELECT * FROM usuarios WHERE tipo = "admin"')
-    if not c.fetchone() and not meu_email:
-        # Só cria o admin@admin se você esquecer de configurar o seu e-mail nos Secrets
-        senha_adm = hashlib.sha256("senha_muito_forte_aqui".encode()).hexdigest()
-        c.execute('INSERT INTO usuarios VALUES (?, ?, ?)', ("admin@admin.com", senha_adm, "admin"))
-        conn.commit()
 
+    c.close()
     conn.close()
 
 def hash_senha(senha):
@@ -41,31 +40,38 @@ def criar_usuario(email, senha):
     conn = conectar()
     c = conn.cursor()
     try:
-        c.execute('INSERT INTO usuarios VALUES (?, ?, ?)', (email, hash_senha(senha), 'user'))
+        c.execute('INSERT INTO usuarios VALUES (%s, %s, %s)', (email, hash_senha(senha), 'user'))
         conn.commit()
         return True
     except:
         return False
     finally:
+        c.close()
         conn.close()
 
 def autenticar_usuario(email, senha):
     conn = conectar()
     c = conn.cursor()
-    c.execute('SELECT * FROM usuarios WHERE email = ? AND senha = ?', (email, hash_senha(senha)))
+    c.execute('SELECT * FROM usuarios WHERE email = %s AND senha = %s', (email, hash_senha(senha)))
     res = c.fetchone()
+    c.close()
     conn.close()
     return res
 
 def registrar_log(usuario, empresa, cargo, score):
-    from datetime import datetime
     conn = conectar()
     c = conn.cursor()
     data = datetime.now().strftime("%d/%m/%Y %H:%M")
-    c.execute("INSERT INTO logs VALUES (?, ?, ?, ?, ?)", (data, usuario, empresa, cargo, score))
+    c.execute("INSERT INTO logs VALUES (%s, %s, %s, %s, %s)", (data, usuario, empresa, cargo, score))
     conn.commit()
+    c.close()
     conn.close()
 
+def buscar_logs():
+    conn = conectar()
+    df = pd.read_sql('SELECT * FROM logs ORDER BY data DESC', conn)
+    conn.close()
+    return df
 def obter_metricas_admin():
     conn = conectar()
     u_count = pd.read_sql("SELECT COUNT(*) as t FROM usuarios", conn).iloc[0]['t']
